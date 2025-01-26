0c88af7286562d1b7739078840807f4e02984938
MLutfi <mlutfi@users-MacBook-Air.local>
2025-01-26 17:06:45 +0700

    initial commit

---
+++ src/main/java/com/mycompany/e/parkir/EParkir.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.e.parkir;
+
+/**
+ *
+ * @author mlutfi
+ */
+public class EParkir {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
